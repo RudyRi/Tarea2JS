f94133963d58aa968fbe66cb42f206fe64412cc0
Rudy Rivera <rudy.andres.rivera@gmail.com>
2023-11-09 15:49:59 -0600

    Primer ejercciio listo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+/*let button =
+document.getElementById("boton");
+button.addEventListener("click", edad);*/
+
 const edad = () => {
-    let num = prompt("Introduzca la edad a consultar: ");
-    num = parseInt(num);
+    //let num = 0;
+    //let num = prompt("Introduzca la edad a consultar: ");
+    let num = document.getElementById('num').value;
+    console.log(num);
     const respuesta = num <18 ? "Esta persona es menor de edad" : "Esta persona es mayor de edad.";
     console.log(respuesta);
+    document.getElementById("respuesta").innerHTML = respuesta;
   };
-
-  edad();
