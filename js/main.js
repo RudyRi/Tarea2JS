7749d8b4d0b231fd6162848e4474a68cc0f0108e
Rudy Rivera <rudy.andres.rivera@gmail.com>
2023-11-10 13:45:45 -0600

    Segundo ejercicio completado

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ const edad = () => {
     console.log(respuesta);
     document.getElementById("respuesta").innerHTML = respuesta;
   };
+
+  const notaFinal = () => {
+    let nombre = document.getElementById('nombre').value;
+    console.log(nombre);
+    let carnet = document.getElementById('carnet').value;
+    console.log(carnet);
+    let examen = parseInt(document.getElementById('examen').value,10);
+    console.log(examen);
+    let tareas = parseInt(document.getElementById('tareas').value,10);
+    console.log(tareas);
+    let asistencia = parseInt(document.getElementById('asistencia').value,10);
+    console.log(asistencia);
+    let investigacion = parseInt(document.getElementById('investigacion').value,10);
+    console.log(investigacion);
+    const suma = examen+tareas+asistencia+investigacion;
+    console.log(suma);
+    const promedio = suma/4;
+    console.log(promedio);
+    respuesta2 = 'Nombre : '+nombre+"<br>"+'Carnet: '+carnet+"<br>"+'Nota Final: '+promedio;
+    console.log(respuesta2);
+    document.getElementById("respuesta2").innerHTML = respuesta2;
+  };
